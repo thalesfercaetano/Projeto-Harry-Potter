44523d59d3857e5891c88e6c1fdb53c7e1537c2f
Luiz Gustavo <luizgustavocontatoprof@gmail.com>
2026-04-10 22:42:51 +0000

    Feat: Adição dos states para afiliação e no sistema de filtragem. Estilização para o titulo e do corpo do container no index.css

--- src/components/FiltrarPersonagens.jsx
+++ src/components/FiltrarPersonagens.jsx
@@ -1,19 +1,29 @@
-const FiltrarPersonagens = ({ busca, setBusca, setCasa }) => {
+const FiltrarPersonagens = ({ busca, setBusca, setCasa, setAfiliacao }) => {
     return (
         <div className="flex flex-wrap gap-4 justify-center items-center mb-10">
-            <input 
-                type="text" 
-                placeholder="Buscar personagem..." 
-                value={busca} 
-                onChange={(e) => setBusca(e.target.value)} 
+            <input
+                type="text"
+                placeholder="Buscar personagem..."
+                value={busca}
+                onChange={(e) => setBusca(e.target.value)}
                 className="bg-amber-100 p-2 rounded-2xl shadow-xl text-slate-900 outline-none w-64 focus:ring-2 ring-amber-500"
             />
-            <div className="flex gap-2 flex-wrap justify-center">
+            <div className="flex gap-2 flex-wrap justify-center items-center">
                 <button className="px-4 py-2 bg-amber-600 text-white rounded cursor-pointer hover:scale-105 transition-transform" onClick={() => setCasa("")}>Todas</button>
                 <button className="px-4 py-2 bg-red-700 text-white rounded cursor-pointer hover:scale-105 transition-transform" onClick={() => setCasa("Grifinória")}>Grifinória</button>
                 <button className="px-4 py-2 bg-green-800 text-white rounded cursor-pointer hover:scale-105 transition-transform" onClick={() => setCasa("Sonserina")}>Sonserina</button>
                 <button className="px-4 py-2 bg-yellow-600 text-white rounded cursor-pointer hover:scale-105 transition-transform" onClick={() => setCasa("Lufa-Lufa")}>Lufa-Lufa</button>
                 <button className="px-4 py-2 bg-blue-800 text-white rounded cursor-pointer hover:scale-105 transition-transform" onClick={() => setCasa("Corvinal")}>Corvinal</button>
+                <select
+                    onChange={(e) => setAfiliacao(e.target.value)}
+                    className="bg-emerald-300 text-center px-3 py-2 rounded-2xl shadow-xl text-slate-900 outline-none cursor-pointer focus:ring-2 ring-emerald-800 hover:scale-102 transition-transform"
+                >
+                    <option value="">Todas as afiliações</option>
+                    <option value="Armada de Dumbledore">Armada de Dumbledore</option>
+                    <option value="Ordem da Fênix">Ordem da Fênix</option>
+                    <option value="Comensais da Morte">Comensais da Morte</option>
+                    <option value="Neutro">Neutro</option>
+                </select>
             </div>
         </div>
     );
--- src/components/PersonagensContainer.jsx
+++ src/components/PersonagensContainer.jsx
@@ -6,20 +6,24 @@ const PersonagensContainer = (props) => {
 
     const [busca, setBusca] = useState("")
     const [casa, setCasa] = useState("")
+    const [afiliacao, setAfiliacao] = useState("")
 
     const exibirPersonagens = props.personagens.filter((personagem)=>{
         const filtroBusca = personagem.nome.toLowerCase().includes(busca.toLowerCase());
         const filtroCasa = casa === "" || personagem.casa === casa
-        return filtroBusca && filtroCasa
+        const filtroAfiliacao = afiliacao === "" || personagem.afiliacao === afiliacao
+        return filtroBusca && filtroCasa && filtroAfiliacao
     })
 
     return(
         <div>
-            <h1>ESCOLA DE MAGIA E BRUXARIA</h1>
+            <h1 className="text-amber-400 text-4xl font-bold text-center tracking-widest uppercase mb-8 drop-shadow-[0_0_10px_rgba(251,191,36,0.5)] mt-4">
+                ESCOLA DE MAGIA E BRUXARIA</h1>
             <FiltrarPersonagens 
                 busca={busca} 
                 setBusca={setBusca} 
                 setCasa={setCasa} 
+                setAfiliacao={setAfiliacao}
             /> 
             <ListaPersonagens personagensFiltrados={exibirPersonagens} />
         </div>
